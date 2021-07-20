f902ef266a3f15d240d5d6f2870f575371f5d1f6
Adryelli <adryellireis0santos@gmail.com>
2021-07-20 16:29:29 -0300

    users router

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,3 +1,4 @@
+import { getCustomRepository } from "typeorm";
 import { UserRepositories } from "../repositories/UsersRepositories";
 
 interface IUserRequest {
@@ -8,7 +9,7 @@ interface IUserRequest {
 
 class CreateUserService{
   async execute({name, email, admin}: IUserRequest) {
-    const userRepository = new UserRepositories();
+    const userRepository = getCustomRepository(UserRepositories);
 
     if (!email) {
       throw new Error("Email incorrect")
