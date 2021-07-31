6402cc98db3c216008d4762d1d44af7375ce6362
Adryelli Reiz <adryellireis0santos@gmail.com>
2021-07-31 01:10:36 -0300

    AuthenticateUserService

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../repositories/UsersRepositories";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+class AuthnticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const usersRepositories = getCustomRepository(UserRepositories);
+
+        const user = await usersRepositories.findOne({
+            email
+        });
+
+        if(!user) {
+            throw new Error("Email/Password incorrect")
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch) {
+            throw new Error("Email/Password incorrect")
+        }
+
+        const token = sign({
+            email: user.email
+        }, "3871e895063b5f4c13c8f90f1b0baf49", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        return token;
+    }
+}
+
+export { AuthnticateUserService }
