8bd0fa5d6b94f6f45be44d92e7b99d8c27ef4183
Adryelli Reiz <adryellireis0santos@gmail.com>
2021-07-31 11:25:28 -0300

    Authenticate user router

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import { CreateTagController } from "./controller/CreateTagController";
 import { CreateUserController } from "./controller/CreateUserController";
+import { AuthenticateUserController } from "./controller/AuthenticateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle);
 
 router.post("/tags", ensureAdmin,createTagController.handle);
+
+router.post("/login", authenticateUserController.handle);
 
 export { router };
