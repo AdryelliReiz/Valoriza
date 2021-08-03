6c34463fa53d0a07377fd58e2deccf1bc96cf17d
Adryelli <adryellireis0santos@gmail.com>
2021-08-02 21:50:52 -0300

    Compliment router

--- src/routes.ts
+++ src/routes.ts
@@ -3,17 +3,21 @@ import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.handle);
 
 router.post("/tags", ensureAdmin,createTagController.handle);
 
 router.post("/login", authenticateUserController.handle);
+
+router.post("/compliment", createComplimentController.handle);
 
 export { router };
